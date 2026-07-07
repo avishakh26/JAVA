cd4ddc8983f47f1a8689c8887bb3928b45494806
Avishakh <studiouspook@gmail.com>
2026-07-08 03:19:05 +0600

    push

--- arman.java
+++ arman.java
@@ -1,5 +1,5 @@
-public static arman{
-    public static void main{String[] args }{
+public class arman{
+    public static void main (String[] args ){
         System.out.println("eka eka korchi eta");
     }
 }

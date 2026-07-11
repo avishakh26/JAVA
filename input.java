ebd28b1902c4c9d74da92baea61c8a2e68811c7c
Avishakh <studiouspook@gmail.com>
2026-07-11 16:02:48 +0600

    gg

--- input.java
+++ input.java
@@ -1,10 +1,28 @@
+// import java.util.*;
+
+// public class input{
+//     public static void main(String[] args){
+
+//         Scanner sc = new Scanner(System.in);
+//         String  name = sc.nextLine();
+//         System.out.println(name);
+//     }
+// }
+
+
+
 import java.util.*;
 
 public class input{
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
-        String  name = sc.next();
+        String  name = sc.nextLine();
+        float number = sc.nextFloat();
+        int intiger = sc.nextInt();
+
         System.out.println(name);
+        System.out.println(number);
+        System.out.println(intiger);
     }
 }

ae32e16e672f8e2dc76ab575f91e70df8c272775
Avishakh <studiouspook@gmail.com>
2026-09-04 10:49:10 +0600

    learning java

--- exa.java
+++ exa.java
@@ -1,9 +1,19 @@
+import java.util.*;
+
 public class exa{
+
     public static void main(String[] args){
 
-        System.out.println("tor ma re chudi");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int age = sc.nextInt();
+        double salary = sc.nextDouble();
 
+
+        System.out.print(name);
+        System.out.println(age);
+        System.out.println(salary);
 
     }
-
 }
+

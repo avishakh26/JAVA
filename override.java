22c993530763a80099740522cbd93578d496aa3b
Avishakh <studiouspook@gmail.com>
2026-09-07 19:30:33 +0600

    All okay. Now understandable

---
+++ override.java
@@ -0,0 +1,26 @@
+class animal{
+    void sounds(){
+        System.out.println("How animal sounds");
+    }
+}
+
+class dog extends animal{
+    @Override
+    void sounds(){
+        System.out.println("Dog do gheu");
+    }
+}
+
+
+
+
+
+public class override{
+    public static void main(String[] args){
+        
+        dog d = new dog();
+        d.sounds();
+        
+
+    }
+}
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,5 +1,1 @@
-public class arman{
-    public static void  main(String[] args){
-        System.out.println("ghurar dim");
-    }
-}
+d.gheu();

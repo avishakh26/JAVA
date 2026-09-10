977ab6448239179615a8faa694a1f7f7afbe7d01
Avishakh <studiouspook@gmail.com>
2026-09-10 19:18:09 +0600

    learning this keyword

--- thischuda.java
+++ thischuda.java
@@ -1,15 +1,24 @@
 class Person {
     String name;
     int age;
+    String hair;
 
     Person(String n, int a) {
         this.name = n;
         this.age = a;
     }
 
+    Person(String n , int a , String h){
+        this.name = n;
+        this.age = a;
+        this.hair = h;
+
+    }
+
     void display() {
         System.out.println("My name is " + name);
         System.out.println("Age is " + age);
+        System.out.println("Hair color is "+ hair);
     }
 }
 
@@ -17,8 +26,10 @@ public class thischuda {
     public static void main(String[] args) {
 
         Person p1 = new Person("Avishakh", 34);
+        Person p2 = new Person("Rahim", 65 , "No hair");
 
         p1.display();
+        p2.display();
         
     }
 }

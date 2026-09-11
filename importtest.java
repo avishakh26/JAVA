2aa62abfaf90d3efb1372b2e621fb615dba64227
Avishakh <studiouspook@gmail.com>
2026-09-11 17:00:24 +0600

    import test

--- importtest.java
+++ importtest.java
@@ -1,7 +1,15 @@
-package university.student;
+import java.util.*;
 
-public class importtest {
-    public void display() {
-        System.out.println("Hello");
+public class importtest{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int age = sc.nextInt();
+        float cg = sc.nextFloat();
+
+        System.out.println("His name is "+name);
+        System.out.println("He is "+age+" years old");
+        System.out.println("His cgpa is "+cg);
+
     }
 }
